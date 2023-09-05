84dc973a750e8806688e50a53b6383ed5d07634a
Kevin.P <kevin.pellissard@gmail.com>
2023-09-05 15:00:27 +0200

    add for the +

--- error.c
+++ error.c
@@ -18,7 +18,11 @@ int _ferror(char *value, unsigned int counter_line)
 
 	for (j = 0; value[j] != '\0'; j++)
 	{
-		if ((value[j] < 48 || value[j] > 57) && value[j] != 45)
+		if (value[j] != 45 || value[j] != 43)
+		{
+			continue;
+		}
+		else if ((value[j] < 48 || value[j] > 57))
 		{
 			fprintf(stderr, "L%d: usage: push integer\n", counter_line);
 			exit(EXIT_FAILURE);
