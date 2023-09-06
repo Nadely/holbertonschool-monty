e4045baabf1f3105234e37ff17c9f729a7caf057
Nadely <6890@holbertonstudents.com>
2023-09-06 14:01:03 +0200

    correction error message in mod function

--- mod.c
+++ mod.c
@@ -16,7 +16,7 @@ void mod(stack_t **stack, unsigned int counter_line)
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't mul, stack too short\n", counter_line);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", counter_line);
 		exit(EXIT_FAILURE);
 	}
 
