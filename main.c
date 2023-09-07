7147d16ff4210467782e67ce4cfff0419f8b12e9
Kevin.P <kevin.pellissard@gmail.com>
2023-09-07 10:04:01 +0200

    Fix the error

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 
 	if (file == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[0]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
